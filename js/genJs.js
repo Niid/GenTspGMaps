9357bcf9a7b12b9568949940aa090b3647c3b26c
Niid <ptitmouls@gmail.com>
2017-11-20 16:56:23 +0100

    Map added

--- js/genJs.js
+++ js/genJs.js
@@ -5,10 +5,41 @@ var currentGen;
 var currentBest;
 var best, bestScore;
 var scores;
+var map;
 
 $( document ).ready(function() {
     // Handler for .ready() called.
+    //init map.
 
-
+    initMap();
+    console.log(map);
+    // This event listener calls addMarker() when the map is clicked.
+    google.maps.event.addListener(map, 'click', function(e) {
+    placeMarker(e.latLng, map);
   });
+});
 
+function initMap() {
+    var grenoble = {lat: 45.188529, lng: 5.724524};
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: grenoble
+    });
+
+    /*var marker = new google.maps.Marker({
+      position: grenoble,
+      map: map
+    })*/
+};
+
+
+
+function placeMarker(position, map) {
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map
+    });  
+    map.panTo(position);
+};
+
+    
