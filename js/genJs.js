adeae05a35736f3e0a4a6b90545ced50cbeece57
Niid <ptitmouls@gmail.com>
2017-11-20 16:04:34 +0100

    Initialisation

---
+++ js/genJs.js
@@ -0,0 +1,14 @@
+var WIDTH;HEIGHT;
+var cities = [];
+
+var currentGen;
+var currentBest;
+var best, bestScore;
+var scores;
+
+$( document ).ready(function() {
+    // Handler for .ready() called.
+
+
+  });
+
